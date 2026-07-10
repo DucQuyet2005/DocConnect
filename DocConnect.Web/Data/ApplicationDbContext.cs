a10eb8528a2c8f7f046b3789ca8877434bbb8429
Quiet <ducquyet2005.work@gmail.com>
2026-07-10 16:11:00 +0700

    init neccessary infor

---
+++ DocConnect.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+namespace DocConnect.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        
+    }
+}
